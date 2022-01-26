dfffafa40880e7568d4d8ad5626e139fee8ee55c
SanujaK29 <kumarags@sheridancollege.ca>
2022-01-26 17:23:11 -0500

    Array size changed from 3 to 4

--- src/student__22/StudentList.java
+++ src/student__22/StudentList.java
@@ -4,7 +4,7 @@ package student__22;
 
 public class StudentList {
     public static void main(String[] args){
-       Student[] studentList = new Student[3];
+       Student[] studentList = new Student[4];
        studentList[0]= new Student("s1","Sanuja Kumarage");
        studentList[1]= new Student("s2","John Cox");
        studentList[2]= new Student("s3","Jessie Holmes");
